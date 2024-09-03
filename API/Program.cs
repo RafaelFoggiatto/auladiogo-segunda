66236a31f4a67fd6ca2a5211754a5eaf4043c38c
Rafael <rafaelfoggiattoDev@gmail.com>
2024-09-02 21:44:56 -0300

    final aula sobre api web

--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,17 @@
 //Minimal APIs em C#
+//Testar API
+// - Rest Client - Extensao VS Code
+// - Postman
+// - Insomnia
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 //funcionalidades 
 //requisicao - url e um metodo/verbo HTTP
-app.MapGet("/", () => "olaaaaaaaaaaa");
+app.MapGet("/", () => "Hello Word em C#");
+
+app.MapGet("/segundafuncionalidade", () => "Segunda funcionalidade");
 
 app.Run();
+//para próxima aula 
+// - criar um endpoint para receber informacao pela URL
